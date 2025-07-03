4614225566d70ae3807fb75e16f2a4637905b5cf
Benoit Bovy <benbovy@gmail.com>
2025-07-03 13:31:02 +0200

    Add TreeAdapter classes for xarray's NDPointIndex

--- src/xoak/__init__.py
+++ src/xoak/__init__.py
@@ -1,10 +1,26 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .accessor import XoakAccessor
-from .index import IndexAdapter, IndexRegistry
+from xoak.accessor import XoakAccessor
+from xoak.index import IndexAdapter, IndexRegistry
+from xoak.tree_adapters import (
+    S2PointTreeAdapter,
+    SklearnBallTreeAdapter,
+    SklearnGeoBallTreeAdapter,
+    SklearnKDTreeAdapter,
+)
 
 try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:  # pragma: no cover
     # package is not installed
     pass
+
+__all__ = [
+    'IndexAdapter',
+    'IndexRegistry',
+    'SklearnBallTreeAdapter',
+    'SklearnGeoBallTreeAdapter',
+    'SklearnKDTreeAdapter',
+    'S2PointTreeAdapter',
+    'XoakAccessor',
+]
