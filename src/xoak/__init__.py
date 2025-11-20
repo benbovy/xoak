0578f6fa66f90d2a10a76734a4ba446cda191218
Benoit Bovy <benbovy@gmail.com>
2025-11-20 13:52:28 +0100

    CI, packaging and doc maintenance (#47)
    * update pre-commit config
    
    Use ruff for linting and formatting.
    
    * run pre-commit on all files
    
    * switch to pyproject / hatchling
    
    * fix test import error
    
    * ci: update pypi publish workflow
    
    * update rtd config
    
    * fix rtc?
    
    * doc env: unpin dependencies
    
    * ci: update test workflow
    
    Skip conda, not really needed here.
    
    * move and refactor conda environment files
    
    * ci tests: switch back to conda (micromamba)
    
    Needed for installing pys2index without hassle.
    
    * add more github config (dependabot, release)
    
    * ci: remove lint workflow (use pre-commit-ci)
    
    * ci: fix inspect package workflow?
    
    * fix pre-commit and some typing fixes
    
    * fix doc build
    
    * doc: switch to sphinx-book theme
    
    * doc env: add missing theme packages

--- src/xoak/__init__.py
+++ src/xoak/__init__.py
@@ -1,14 +1,12 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    # Python < 3.8
-    from importlib_metadata import version, PackageNotFoundError
+from importlib.metadata import version
 
 from .accessor import XoakAccessor
 from .index import IndexAdapter, IndexRegistry
 
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:  # pragma: no cover
-    # package is not installed
-    pass
+__all__ = [
+    "XoakAccessor",
+    "IndexAdapter",
+    "IndexRegistry",
+]
+
+__version__ = version("xoak")
